b995ece3e02423d7cef0843dffed8e7b322b1739
Evan Teran <evan.teran@gmail.com>
2015-04-03 20:34:38 -0400

    adding find_last algorithms

--- bitset.hpp
+++ bitset.hpp
@@ -8,18 +8,34 @@ namespace bitset {
 
 template <size_t N>
 int find_first(const std::bitset<N> &bs) {
-		if(bs.none()) {
-			return N;
-		}
+	if(bs.none()) {
+		return N;
+	}
 
-		std::size_t i;
-		for(i = 0; i < N; ++i) {
-			if(bs[i]) {
-				break;
-			}
+	std::size_t i;
+	for(i = 0; i < N; ++i) {
+		if(bs[i]) {
+			break;
 		}
+	}
 
-		return i;	
+	return i;	
+}
+
+template <size_t N>
+int find_last(const std::bitset<N> &bs) {
+	if(bs.none()) {
+		return N;
+	}
+	
+	size_t i = N;
+	while(--i) {
+		if(bs[i]) {
+			break;
+		}
+	}
+	
+	return i;
 }
 
 #if defined(__GNUC__)
@@ -30,6 +46,15 @@ inline int find_first(const std::bitset<32> &bs) {
 	}
 	return __builtin_ctz(static_cast<uint32_t>(bs.to_ulong()));	
 }
+
+template <>
+inline int find_last(const std::bitset<32> &bs) {
+	if(bs.none()) {
+		return bs.size();
+	}
+	return bs.size() - __builtin_clz(static_cast<uint32_t>(bs.to_ulong())) - 1;
+}
+
 #if __cplusplus >= 201103L
 template <>
 inline int find_first(const std::bitset<64> &bs) {
@@ -37,6 +62,14 @@ inline int find_first(const std::bitset<64> &bs) {
 		return bs.size();
 	}
 	return __builtin_ctzll(static_cast<uint64_t>(bs.to_ullong()));
+}
+
+template <>
+inline int find_last(const std::bitset<64> &bs) {
+	if(bs.none()) {
+		return bs.size();
+	}
+	return bs.size() - __builtin_clzll(static_cast<uint64_t>(bs.to_ullong())) - 1;
 }
 #endif
 #endif
