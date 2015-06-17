8b3764388db9407023876ac973b5771291b73635
Evan Teran <evan.teran@gmail.com>
2015-06-17 02:38:46 -0400

    making trim utilities use safe_ctype stuff

--- string.hpp
+++ string.hpp
@@ -53,14 +53,14 @@ inline bool starts_with(const std::string &s, const std::string &prefix) {
 // Name: rtrim
 //------------------------------------------------------------------------------
 inline void rtrim(std::string &s) {
-	s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+	s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun(safe_ctype<std::isspace>))).base(), s.end());
 }
 
 //------------------------------------------------------------------------------
 // Name: ltrim
 //------------------------------------------------------------------------------
 inline void ltrim(std::string &s) {
-	s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+	s.erase(s.begin(), find_if(s.begin(), s.end(), std::not1(std::ptr_fun(safe_ctype<std::isspace>))));
 }
 
 //------------------------------------------------------------------------------
