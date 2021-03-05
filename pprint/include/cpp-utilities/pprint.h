a9da24aa37e78c8dfc191f05a46d93d3880478bb
Evan Teran <evan.teran@gmail.com>
2021-03-05 14:36:32 -0500

    fix header guard

--- pprint/include/cpp-utilities/pprint.h
+++ pprint/include/cpp-utilities/pprint.h
@@ -1,18 +1,18 @@
 /*
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2015 Evan Teran
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,9 +21,9 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
- 
+
 #ifndef UTILITIES_PPRINT_HPP_
-#define UTILITIES_STRING_HPP_
+#define UTILITIES_PPRINT_HPP_
 
 #include <deque>
 #include <iomanip>
@@ -58,7 +58,7 @@ template <class C>
 std::string to_string_container(const std::string &type, const C &c, int indent) {
 	std::stringstream ss;
 	typename C::size_type i = 0;
-	
+
 	ss << std::setw(indent * 4) << type << std::endl;
 	ss << std::setw(indent * 4) << "(" << std::endl;
 	++indent;
@@ -73,7 +73,7 @@ std::string to_string_container(const std::string &type, const C &c, int indent)
 template <class C>
 std::string to_string_assoc_container(const std::string &type, const C &c, int indent) {
 	std::stringstream ss;
-	
+
 	ss << std::setw(indent * 4) << type << std::endl;
 	ss << std::setw(indent * 4) << "(" << std::endl;
 	++indent;
